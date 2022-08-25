e53b1aeb024056e18570ef80673668b748011c7e
Deep @BigDataCloud <deep@bigdatacloud.com>
2022-08-25 13:34:33 +0930

    CNAME

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
     <div class="half">
       <h1>DBS Design Studio</h1>
       <p class="caption-txt">mind of an engineer,<br />heart of an artist.</p>
-      <NuxtLink to="about">About Us</NuxtLink>
     </div>
     <div class="half title-txt">
       A software design company; imagining and building products and
